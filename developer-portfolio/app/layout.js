330fc7c7f98709516d0c98b7a412ef836996c24f
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2025-09-22 22:12:13 -0400

    update developer-portfolio

--- developer-portfolio/app/layout.js
+++ developer-portfolio/app/layout.js
@@ -4,8 +4,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
-import "./css/card.scss";
-import "./css/globals.scss";
+import "./css/card.css";
+import "./css/globals.css";
 import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
